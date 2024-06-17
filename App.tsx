3dd5aac04289f63bbe8f0fd605923985a944ac04
betiniakarandut <betiniakarandut@gmail.com>
2024-06-17 14:15:48 +0100

    feat: new components

--- App.tsx
+++ App.tsx
@@ -1,53 +1,23 @@
+// src/App.tsx
+
 import React from 'react';
-import { SafeAreaView, StyleSheet, FlatList, ListRenderItem } from 'react-native';
-import VideoItem from './src/components/video/VideoItem';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { RootStackParamList } from './src/types/navigation';
+import AuthStack from './src/navigation/AuthStack';
+import HomeStack from './src/navigation/HomeStack';
 
-interface Video {
-  id: string;
-  thumbnail: string;
-  title: string;
-  channelName: string;
-  views: number;
-}
-
-const videos: Video[] = [
-  {
-    id: '1',
-    thumbnail: 'https://example.com/thumbnail1.jpg',
-    title: 'Video 1',
-    channelName: 'Channel 1',
-    views: 1000,
-  },
-  {
-    id: '2',
-    thumbnail: 'https://example.com/thumbnail2.jpg',
-    title: 'Video 2',
-    channelName: 'Channel 2',
-    views: 2000,
-  },
-];
+const RootStack = createStackNavigator<RootStackParamList>();
 
 const App = () => {
-  const renderItem: ListRenderItem<Video> = ({ item }) => (
-    <VideoItem video={item} onPress={(video) => console.log('Pressed', video)} />
-  );
-
   return (
-    <SafeAreaView style={styles.container}>
-      <FlatList
-        data={videos}
-        renderItem={renderItem}
-        keyExtractor={(item) => item.id}
-      />
-    </SafeAreaView>
+    <NavigationContainer>
+      <RootStack.Navigator>
+        <RootStack.Screen name="Auth" component={AuthStack} />
+        <RootStack.Screen name="Home" component={HomeStack} />
+      </RootStack.Navigator>
+    </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'linear-gradient(to right, rgb(203, 16, 16), rgb(178, 36, 58))',
-  },
-});
 
 export default App;
