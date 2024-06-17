ecdd110d8a562ce16d3668387b27c4240b295290
betiniakarandut <betiniakarandut@gmail.com>
2024-06-17 14:10:17 +0100

    feat: Auth stack

---
+++ src/navigation/AuthStack.tsx
@@ -0,0 +1,19 @@
+// src/navigations/AuthStack.tsx
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import { AuthStackParamList } from '../types/navigation';
+import SignInScreen from '../screens/SignInScreen';
+import SignUpScreen from '../screens/SignUpScreen';
+
+const Stack = createStackNavigator<AuthStackParamList>();
+
+const AuthStack = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="SignIn" component={SignInScreen} />
+      <Stack.Screen name="SignUp" component={SignUpScreen} />
+    </Stack.Navigator>
+  );
+};
+
+export default AuthStack;
