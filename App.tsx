4ec455db67a1e0dad7646cbb4a7cbd405eca86c8
betiniakarandut <betiniakarandut@gmail.com>
2024-06-20 21:13:20 +0100

    feat: App

--- App.tsx
+++ App.tsx
@@ -3,16 +3,16 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { RootStackParamList } from './src/types/navigation';
 import AuthStack from './src/navigation/AuthStack';
 import HomeStack from './src/navigation/HomeStack';
+import { RootStackParamList } from './src/types/navigation';
 
 const RootStack = createStackNavigator<RootStackParamList>();
 
-const App = () => {
+const App: React.FC = () => {
   return (
     <NavigationContainer>
-      <RootStack.Navigator>
+      <RootStack.Navigator screenOptions={{ headerShown: false }}>
         <RootStack.Screen name="Auth" component={AuthStack} />
         <RootStack.Screen name="Home" component={HomeStack} />
       </RootStack.Navigator>
