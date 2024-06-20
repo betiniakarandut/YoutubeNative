16f2212ba5735440b8e5999cedbe4f156c72f245
betiniakarandut <betiniakarandut@gmail.com>
2024-06-20 21:10:06 +0100

    feat: components, screens, types

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,12 +1,17 @@
+// api/auth.ts
 import apiClient from './apiClient';
-import { User } from '../types/user';
 
 export const signIn = async (email: string, password: string) => {
   const response = await apiClient.post('/user/signin', { email, password });
-  return response.data;
+  return response;
 };
 
-export const signUp = async (email: string, password: string) => {
-  const response = await apiClient.post('/user/signup', { email, password });
-  return response.data;
+export const signUp = async (username: string, email: string, phone: string, password: string) => {
+  const response = await apiClient.post('/user/signup', { username, email, phone, password });
+  return response;
+};
+
+export const verifyOTP = async (email: string, otp: string) => {
+  const response = await apiClient.post('/user/verifyotp', { email, otp });
+  return response;
 };
