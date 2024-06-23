950f64fdbf7736a8598a785b042d0be465b14171
betiniakarandut <betiniakarandut@gmail.com>
2024-06-24 00:11:33 +0100

    feat: RootStackParamList

--- src/navigation/HomeStack.tsx
+++ src/navigation/HomeStack.tsx
@@ -2,10 +2,17 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from '../screens/HomeScreen';
 import SearchScreen from '../screens/SearchScreen';
-import { HomeStackParamList } from '../types/navigation';
 import ProfileScreen from '../screens/ProfileScreen';
+import VideoDetailsScreen from '../screens/VideoDetailsScreen';
 
-const Stack = createStackNavigator<HomeStackParamList>();
+type RootStackParamList = {
+  Home: undefined;
+  Search: undefined;
+  Profile: undefined;
+  VideoDetails: { videoId: string };
+};
+
+const Stack = createStackNavigator<RootStackParamList>();
 
 const HomeStack = () => {
   return (
@@ -13,6 +20,7 @@ const HomeStack = () => {
       <Stack.Screen name="Home" component={HomeScreen} />
       <Stack.Screen name="Search" component={SearchScreen} />
       <Stack.Screen name="Profile" component={ProfileScreen} />
+      <Stack.Screen name="VideoDetails" component={VideoDetailsScreen} />
     </Stack.Navigator>
   );
 };
