237018bb6b1776096a1b62814561ded704345b72
betiniakarandut <betiniakarandut@gmail.com>
2024-07-01 16:38:30 +0100

    feat: video url endpoint

--- src/api/video.ts
+++ src/api/video.ts
@@ -1,5 +1,5 @@
 import apiClient from './apiClient';
-import { Video } from '../types/video';
+// import { Video } from '../types/video';
 
 export const getVideos = async () => {
   const response = await apiClient.get('/videos');
