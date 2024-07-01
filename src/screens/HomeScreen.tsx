2a45497c222ff17d8b1efa76cc13c467d19634d0
betiniakarandut <betiniakarandut@gmail.com>
2024-07-01 16:37:54 +0100

    feat: load videos

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,37 +1,39 @@
-// screens/HomeScreen.tsx
-import React, { useEffect, useState } from 'react';
-import { View, Text, FlatList } from 'react-native';
-import { Video } from '../types/video';
-import apiClient, { setAuthToken } from '../api/apiClient';
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import ProfileScreen from './ProfileScreen';
+import SearchScreen from './SearchScreen';
+import VideoList from './VideoList';
+import { NavigationContainer } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+const Tab = createBottomTabNavigator();
 
 const HomeScreen: React.FC = () => {
-  const [videos, setVideos] = useState<Video[]>([]);
-
-  useEffect(() => {
-    const fetchVideos = async () => {
-      try {
-        const token = setAuthToken('token');
-        const response = await apiClient('/videos', {
-          headers: { Authorization: `Bearer ${token}` },
-        });
-        setVideos(response.data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    fetchVideos();
-  }, []);
-
   return (
-    <View>
-      <Text>Home</Text>
-      <FlatList
-        data={videos}
-        keyExtractor={(item) => item._id}
-        renderItem={({ item }) => <Text>{item.title}</Text>}
-      />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color, size }) => {
+            let iconName;
+
+            if (route.name === 'Profile') {
+              iconName = 'ios-person';
+            } else if (route.name === 'Search') {
+              iconName = 'ios-search';
+            } else if (route.name === 'Home') {
+              iconName = 'ios-home';
+            }
+
+            return <Icon name={iconName} size={size} color={color} />;
+          },
+          tabBarLabel: route.name,
+        })}
+      >
+        <Tab.Screen name="Home" component={VideoList} />
+        <Tab.Screen name="Search" component={SearchScreen} />
+        <Tab.Screen name="Profile" component={ProfileScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 };
 
